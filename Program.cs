053135436fe7adc88eecdceb72c7cff8e211548c
aAibdsc <153871791+aAibdsc@users.noreply.github.com>
2023-12-15 00:05:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listPOO10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite o peso da pessoa (em kg): ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a altura da pessoa (em metros): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(peso, altura);
+            string status = pessoa.CalcularStatus();
+
+            Console.WriteLine($"A pessoa está com o status: {status}");
+        }
+    }
+}
